a2a5179d4dc5042b85210c651376223f00c71916
Jose Rosario <chayo777@msn.com>
2017-07-20 17:05:59 -0500

    Comit 1

---
+++ MiUber/MiUberAndroid/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MiUberAndroid
+{
+
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Login : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Login);
+            var txtForgotPassword = FindViewById<TextView>(Resource.Id.txtPasswordForgot);
+            var txtSignIn = FindViewById<TextView>(Resource.Id.txtSignIn);
+            txtForgotPassword.Click += (sender, evt) =>
+            {
+                var intent = new Android.Content.Intent(this, typeof(ForgotPassword));
+                  StartActivity(intent);
+            };
+            txtSignIn.Click += (sender, evt) =>
+            {
+                var intent = new Android.Content.Intent(this, typeof(ValidateNumberPhone));
+                StartActivity(intent);
+            };
+
+
+
+        }
+    }
+}
---
+++ MiUber/MiUberAndroid/ValidateNumberPhone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MiUberAndroid
+{
+    [Activity(Label = "ValidateNumberPhone")]
+    public class ValidateNumberPhone : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ValidateNumberPhone);
+
+            var btnSendCode = FindViewById<Button>(Resource.Id.btnSendCode);
+            var btnNext = FindViewById<Button>(Resource.Id.btnNext);
+
+            btnSendCode.Click += (sender, evd) =>
+            {
+                btnNext.Enabled = true;
+            };
+
+            btnNext.Click += (sender, evt) => 
+            {
+                var intent = new Android.Content.Intent(this, typeof(SignIn));
+                StartActivity(intent);
+            };
+            
+
+            // Create your application here
+        }
+    }
+}
